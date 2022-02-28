88ffdc54a98c2a8dbf2e4914790e7ab8b46286b2
Mike <mikej.miller440@gmail.com>
2022-02-28 17:46:59 -0600

    Added CRUD for comments

--- controllers/apiRoutes/comment-routes.js
+++ controllers/apiRoutes/comment-routes.js
@@ -58,4 +58,53 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
+router.post('/', async (req, res) => {
+	try {
+		const dbCommentData = await Comment.create({
+			comment_text: req.body.comment_text,
+			user_id: req.body.user_id,
+			post_id: req.body.post_id
+		});
+		res.json(dbCommentData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	try {
+		const dbCommentData = await Comment.update(req.body, {
+			where: {
+				id: req.params.id
+			}
+		});
+		if (!dbCommentData) {
+			return res.status(404).json({
+				message: `A comment with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbCommentData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const dbCommentData = await Comment.destroy({
+			where: {
+				id: req.params.id
+			}
+		});
+		if (!dbCommentData) {
+			res.status(404).json({
+				message: `A comment with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbCommentData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
