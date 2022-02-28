14b428b4c423daa1999112b718646be798171cf2
Mike <mikej.miller440@gmail.com>
2022-02-28 11:19:09 -0600

    edited model relationships

---
+++ models/index.js
@@ -0,0 +1,33 @@
+const Comment = require('./Comment');
+const Post = require('./Post');
+const User = require('./User');
+
+User.hasMany(Post, {
+	foreignKey: 'post_id'
+});
+
+Post.belongsTo(User, {
+	foreignKey: 'post_id',
+	onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(User, {
+	foreignKey: 'user_id',
+	onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+	foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+	foreignKey: 'post_id',
+	onDelete: 'SET NULL'
+});
+
+Post.hasMany(Comment, {
+	foreignKey: 'post_id',
+	onDelete: 'SET NULL'
+});
+
+module.exports = { Comment, Post, User };
