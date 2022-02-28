f6225232125430783d0816d5f865861bbb85d125
Mike <mikej.miller440@gmail.com>
2022-02-28 17:14:43 -0600

    added POST and PUSH endpoint

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -35,4 +35,36 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
+router.post('/', async (req, res) => {
+	try {
+		const dbUserData = await User.create({
+			username: req.body.username,
+			email: req.body.email,
+			password: req.body.password
+		});
+		res.json(dbUserData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	try {
+		const dbUserData = await User.update(req.body, {
+			individualHooks: true,
+			where: {
+				id: req.params.id
+			}
+		});
+		if (!dbUserData) {
+			return res.status(404).json({
+				message: `A user with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbUserData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
