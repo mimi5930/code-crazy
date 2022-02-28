7abff10435556ea7e4631158771cc2e3cab39ce7
Mike <mikej.miller440@gmail.com>
2022-02-28 17:29:15 -0600

    added CRUD for Posts

--- controllers/apiRoutes/post-routes.js
+++ controllers/apiRoutes/post-routes.js
@@ -38,4 +38,51 @@ router.get('/:id', async (req, res) => {
 	}
 });
 
+router.post('/', async (req, res) => {
+	try {
+		const dbPostData = await Post.create({
+			title: req.body.title,
+			user_id: req.body.user_id
+		});
+		res.json(dbPostData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.put('/:id', async (req, res) => {
+	try {
+		const dbPostData = await Post.update(req.body, {
+			where: {
+				id: req.params.id
+			}
+		});
+		if (!dbPostData) {
+			return res.status(404).json({
+				message: `A post with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbPostData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.delete('/:id', async (req, res) => {
+	try {
+		const dbPostData = await Post.destroy({
+			where: {
+				id: req.params.id
+			}
+		});
+		if (!dbPostData) {
+			return res.status(404).json({
+				message: `A post with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbPostData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
 module.exports = router;
