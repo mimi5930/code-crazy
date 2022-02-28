8c6fef3e68cb792a071baf87bcb92f6552c14422
Mike <mikej.miller440@gmail.com>
2022-02-28 17:19:21 -0600

    added DELETE route

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -67,4 +67,22 @@ router.put('/:id', async (req, res) => {
 	}
 });
 
+router.delete('/:id', async (req, res) => {
+	try {
+		const dbUserData = await User.destroy({
+			where: {
+				id: req.params.id
+			}
+		});
+		if (!dbUserData) {
+			return res.status(404).json({
+				message: `A user with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbUserData);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
 module.exports = router;
