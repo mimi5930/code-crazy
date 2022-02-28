211740cc918b79a1a7d1b2358ebd350293973e6d
Mike <mikej.miller440@gmail.com>
2022-02-28 11:59:34 -0600

    added single user get

--- controllers/apiRoutes/user-routes.js
+++ controllers/apiRoutes/user-routes.js
@@ -14,4 +14,25 @@ router.get('/', async (req, res) => {
 	}
 });
 
+router.get('/:id', async (req, res) => {
+	try {
+		const dbUserData = await User.findOne({
+			where: {
+				id: req.params.id
+			},
+			attributes: {
+				exclude: ['password']
+			}
+		});
+		if (!dbUserData) {
+			return res.status(404).json({
+				message: `A user with an id of ${req.params.id} was not found!`
+			});
+		}
+		res.json(dbUserData);
+	} catch (err) {
+		res.status(500);
+	}
+});
+
 module.exports = router;
