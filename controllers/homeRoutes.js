5b19f07d87313a2b8da9bbebeead299aacc264f7
Mike <mikej.miller440@gmail.com>
2022-02-28 22:49:12 -0600

    added homepage and handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,3 +1,28 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+
+router.get('/', async (req, res) => {
+	const dbPostData = await Post.findAll({
+		attributes: ['id', 'post_url', 'title', 'created_at'],
+		include: [
+			{
+				model: Comment,
+				attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+				include: {
+					model: User,
+					attributes: ['username']
+				}
+			},
+			{
+				model: User,
+				attributes: 'username'
+			}
+		]
+	});
+	const posts = dbPostData.map(post => post.get({ plain: true }));
+
+	res.render('homepage', {
+		posts
+	});
+});
