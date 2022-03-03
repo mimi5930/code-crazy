9976ccd1fa912d8ecfbdbea4991cfbf196e821a4
Mike <mikej.miller440@gmail.com>
2022-03-03 13:49:36 -0600

    added login javascript

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,8 +1,29 @@
 const loginform = document.querySelector('.login-form');
 
-const loginFormHandler = event => {
-	console.log('clicked!');
+const loginFormHandler = async event => {
 	event.preventDefault();
+
+	const email = document.querySelector('#email-login').value.trim();
+	const password = document.querySelector('#password-login').value.trim();
+
+	if (email && password) {
+		const res = await fetch('/api/users/login', {
+			method: 'post',
+			body: JSON.stringify({
+				email,
+				password
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+
+		if (res.ok) {
+			document.location.replace('/');
+		} else {
+			alert(res.statusText);
+		}
+	}
 };
 
 loginform.addEventListener('submit', loginFormHandler);
