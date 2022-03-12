f601284fae1368b00ba448e7ef2e4a008715ec3f
Mike <mikej.miller440@gmail.com>
2022-03-11 21:50:20 -0600

    added sessions

--- server.js
+++ server.js
@@ -1,25 +1,26 @@
 const path = require('path');
 const express = require('express');
-// const session = require('express-session');
+const session = require('express-session');
 const exphbs = require('express-handlebars');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// const sess = {
-// 	sectret: 'Super duper secret',
-// 	cookie: {},
-// 	resave: false,
-// 	saveUninitialized: true,
-// 	store: new SequelizeStore({
-// 		db: sequelize
-// 	})
-// };
+const sess = {
+	secret: process.env.SECRET,
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize
+	})
+};
 
-// app.use(session(sess));
+app.use(session(sess));
 
 const hbs = exphbs.create({});
 
